5af36afbb4902770f82d58f51421dd01c81b5b23
Saumy-TOXOTIS <saumytiwari95@gmail.com>
2025-11-19 22:31:20 +0530

    minor bug fix

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -25,7 +25,10 @@ const registerUser = asyncHandler(async (req, res) => {
     //check for images, check for avatar
     console.log("req.files: ", req.files);
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
     if(!avatarLocalPath) {
         throw new ApiError(400, "Avatar image is required");
     }
